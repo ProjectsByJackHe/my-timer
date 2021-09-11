b8bfdfeebc1e81fa93630f001b326a263ff7e8f2
Jack He <43735701+ProjectsByJackHe@users.noreply.github.com>
2021-09-11 16:56:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const audioPlayer = document.getElementById("audio-player")
+
+document.getElementById("start-timer").onclick = () => {
+    let duration = document.getElementById("duration").value 
+    let song = document.getElementById("song").value 
+
+    audioPlayer.src = "/music/" + song
+    document.title = "active timer - " + song
+    let seconds = duration * 60;
+    let future = new Date().getTime() + (duration * 1000 * 60)
+    let timerPtr = setInterval(() => {
+        document.getElementById("label").innerHTML = String(seconds) + " Seconds Remaining"
+        seconds -= 1;
+    }, 1000);
+
+    function on() {
+        if(document.hidden) {
+            // tab is now inactive
+            // temporarily clear timer using clearInterval() / clearTimeout()
+            clearInterval(timerPtr)
+        }
+        else {
+            // tab is active again
+            // restart timers
+            // update duration
+            let now = new Date().getTime()
+            let diff = future - now 
+            seconds = Math.floor(diff / 1000)
+            timerPtr = setInterval(() => {
+                document.getElementById("label").innerHTML = String(seconds) + " Seconds Remaining"
+                seconds -= 1;
+            }, 1000)
+        }
+    }
+    
+    document.addEventListener('visibilitychange', on);
+
+    setTimeout(() => {
+        audioPlayer.play()
+        clearInterval(timerPtr)
+        document.title = "timer" 
+        document.getElementById("label").innerHTML = "0 Seconds Remaining"
+        document.removeEventListener("visibilitychange", on)
+    }, duration * 1000 * 60);
+}
